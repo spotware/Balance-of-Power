5337b7ed7410de157d599c5565d394e669cf48af
Ahmad Noman Musleh <afhackermubasher@gmail.com>
2021-09-07 09:34:13 +0300

    Add project files.

---
+++ Balance of Power/Balance of Power.cs
@@ -0,0 +1,35 @@
+﻿using cAlgo.API;
+using cAlgo.API.Indicators;
+
+namespace cAlgo
+{
+    [Indicator(IsOverlay = false, TimeZone = TimeZones.UTC, AccessRights = AccessRights.None)]
+    public class BalanceofPower : Indicator
+    {
+        private IndicatorDataSeries _bop;
+        private MovingAverage _ma;
+
+        [Parameter("Periods", DefaultValue = 14)]
+        public int Periods { get; set; }
+
+        [Parameter("MA Type", DefaultValue = MovingAverageType.Simple)]
+        public MovingAverageType MaType { get; set; }
+
+        [Output("Main")]
+        public IndicatorDataSeries Result { get; set; }
+
+        protected override void Initialize()
+        {
+            _bop = CreateDataSeries();
+            _ma = Indicators.MovingAverage(_bop, Periods, MaType);
+        }
+
+        public override void Calculate(int index)
+        {
+            var bar = Bars[index];
+
+            _bop[index] = (bar.Close - bar.Open) / (bar.High - bar.Low);
+            Result[index] = _ma.Result[index];
+        }
+    }
+}
